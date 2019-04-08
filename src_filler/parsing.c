a713d56aea237ac48c897e5d0f4164d847ee4e7e
Philippine SENTILHES <psentilh@e1r13p5.42.fr>
2019-04-08 19:44:06 +0200

    draft

--- src_filler/parsing.c
+++ src_filler/parsing.c
@@ -6,14 +6,27 @@
 /*   By: psentilh <psentilh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 17:38:59 by psentilh          #+#    #+#             */
-/*   Updated: 2019/04/08 19:34:46 by psentilh         ###   ########.fr       */
+/*   Updated: 2019/04/08 19:43:37 by psentilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
+char	**check_map(t_map *map, int fd)
+{
+	char **tmp[BUFF_SIZE + 1];
+	int ret;
+
+	ret = 0;
+	tmp = NULL;
+	if ((ret = read(fd, tmp, BUFF_SIZE)) == -1)
+		return (NULL);
+	tmp[ret] = '\0';
+	return (tmp);
+}
+
 int		parsing(t_map *map, int fd)
 {
-
+	map->map = check_map(map, fd);
 	return (0);
 }
